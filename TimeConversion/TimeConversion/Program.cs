d2f6069ada81393d3938154290c67e36f8dd687c
samar dahy <samar.dahy@amwal-pay.com>
2024-10-22 04:37:11 +0300

    push

---
+++ TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+
+
+TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+string s = Console.ReadLine();
+
+string result = Result.timeConversion(s);
+
+textWriter.WriteLine(result);
+
+textWriter.Flush();
+textWriter.Close();
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        var result = Convert.ToDateTime(s);
+        string convertTo12Format = result.ToString("hh:mm:ss", CultureInfo.CurrentCulture); // to 12
+        string convertTo24Format = result.ToString("HH:mm:ss", CultureInfo.CurrentCulture); // to 24
+        return convertTo24Format;
+    }
+
+}
+
+
